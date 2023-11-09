ccbcebd2f0b9de7d972720a58b4f5f922c8c3a20
FarhanKabiri <fkabiri.inqs@gmail.com>
2023-11-09 00:58:45 -0500

    changed acc info

--- QVerse.py
+++ QVerse.py
@@ -50,7 +50,7 @@ def create_image_with_verse(verse_data):
 # Automate posts on Instagram
 def post_to_instagram():
     user_name = os.getenv('INSTAGRAM_USERNAME','reminder_of_akh1ra')
-    password = os.getenv('INSTAGRAM_PASSWORD','Iownthis@1')
+    password = os.getenv('INSTAGRAM_PASSWORD','Iownthis313')
 
     caption = 'Assalamualaykum, Here is a random verse from the Quran.'
     png_image_path = 'random_verse.png'
